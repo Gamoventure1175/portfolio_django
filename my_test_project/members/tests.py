337e3643b7fa5f4f3e31853fa210f5b3f2018f24
unknown <Gaurav Abhiman Mahajan>
2025-07-15 18:52:53 +0530

    ran unit test using TestCase class of django.test which involved checking databases as well

--- my_test_project/members/tests.py
+++ my_test_project/members/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.http import HttpResponse
 
-# Create your tests here.
+# Creating simple tests to chekc 200 status codes for our 'members', 'home'and 'sample' page
+
+
+class MemberPageTest(TestCase):
+    def test_member_page_url(self):
+        response: HttpResponse = self.client.get("/members/")
+        self.assertEqual(response.status_code, 200)
+
+
+class SamplePageTest(TestCase):
+    def test_member_page_url(self):
+        response: HttpResponse = self.client.get("/sample/")
+        self.assertEqual(response.status_code, 200)
+
+
+class HomePageTest(TestCase):
+    def test_member_page_url(self):
+        response: HttpResponse = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
