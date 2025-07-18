549fbadca0e491fd20e2401ad67892548343a89a
Gamoventure1175 <gauravmahajan1175@gmail.com>
2025-07-19 00:10:46 +0530

    minor change in the naming detail

--- my_test_project/members/tests.py
+++ my_test_project/members/tests.py
@@ -36,16 +36,23 @@ class AboutPageTest(
         )
 
     def test_model_content(self):
-        """Adding 3 test together to continue the DRY approach of Django"""
+        """## Test case to check the contents of the database model like:
+        1. firstname
+        2. lastname
+        3. phone number
+
+
+        ***Adding 3 test cases together to continue the DRY approach of Django***"""
         self.assertEqual(self.member.firstname, "John")
         self.assertEqual(self.member.lastname, "Doe")
         self.assertEqual(self.member.phone, 8888888888)
 
     def test_about_page_url(self):
+        """Test case to check if the url "/about/" is working correctly"""
         response: HttpResponse = self.client.get("/about/")
         self.assertEqual(response.status_code, 200)
 
-    def test_about_page_detials(self):
+    def test_about_page_details(self):
         """## Test case to check the details of the page like:
         1. The 'about' name is assigned to the url '/about/'
         2. The right template is being used: pages/about.html
