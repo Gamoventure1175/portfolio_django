4ab87a644635ea09c7ff6366511f9e7642835c4e
unknown <Gaurav Abhiman Mahajan>
2025-07-18 15:06:09 +0530

    restructured the tests, used a class method, grouped similar tests into single test functions

--- my_test_project/members/tests.py
+++ my_test_project/members/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, SimpleTestCase
 from django.http import HttpResponse
 from django.urls import reverse
+from .models import Member
 
 # Creating simple tests to chekc 200 status codes for our 'members', 'home'and 'sample' page
 
@@ -28,19 +29,29 @@ class HomePageTest(TestCase):
 class AboutPageTest(
     TestCase
 ):  # about uses db, so SimpleTestCase won't be able to handle this scenario
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.member = Member.objects.create(
+            firstname="John", lastname="Doe", phone=8888888888
+        )
+
+    def test_model_content(self):
+        """Adding 3 test together to continue the DRY approach of Django"""
+        self.assertEqual(self.member.firstname, "John")
+        self.assertEqual(self.member.lastname, "Doe")
+        self.assertEqual(self.member.phone, 8888888888)
+
     def test_about_page_url(self):
         response: HttpResponse = self.client.get("/about/")
         self.assertEqual(response.status_code, 200)
 
-    def test_about_name_available(self):
-        """Test case to check if the name "about" is available for this url route"""
+    def test_about_page_detials(self):
+        """## Test case to check the details of the page like:
+        1. The 'about' name is assigned to the url '/about/'
+        2. The right template is being used: pages/about.html
+        3. The page contains the right content: 'Gamoventure'
+        """
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
-
-    def test_html_template_name(self):
-        response = self.client.get(reverse("about"))
         self.assertTemplateUsed(response, "pages/about.html")
-
-    def test_html_contains_company_name(self):
-        response = self.client.get(reverse("about"))
-        self.assertContains(response, "<h1>Gamoventure</h1>")
+        self.assertContains(response, "Gamoventure")
