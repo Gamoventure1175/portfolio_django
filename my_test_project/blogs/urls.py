213a2f376bf6ccfee6a7e7463e21d949abb4f4bd
Gamoventure1175 <gauravmahajan1175@gmail.com>
2025-07-20 14:14:28 +0530

    added pages for user blogs using named urls and the Blog model

--- my_test_project/blogs/urls.py
+++ my_test_project/blogs/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import all_blogs
+from .views import all_blogs, blog_details
 
-urlpatterns = [path("blogs/", all_blogs, name="blogs")]
+urlpatterns = [
+    path("blogs/", all_blogs, name="blogs"),
+    path("blogs/<int:pk>/", blog_details, name="blog_detail"),
+]
--- my_test_project/blogs/views.py
+++ my_test_project/blogs/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest
+from .models import Blog
 
 
 # Create your views here.
 def all_blogs(request: HttpRequest):
-    return HttpResponse("Hello World")
+    blogs = Blog.objects.all()
+    context = {"blogs": blogs}
+    return render(request, "blogs/home.html", context=context)
+
+
+def blog_details(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    context = {"blog": blog}
+    return render(request, template_name="blogs/blog_detail.html", context=context)
