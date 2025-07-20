1edd1b90c55ebc08e1cbb2a54486b6f06a70b5e5
Gamoventure1175 <gauravmahajan1175@gmail.com>
2025-07-20 13:12:48 +0530

    changes to the Blog model and a little refinement on the admin panel

--- my_test_project/blogs/admin.py
+++ my_test_project/blogs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Blog
+
 
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "body"]
+
+
+admin.site.register(Blog, BlogAdmin)
--- my_test_project/blogs/models.py
+++ my_test_project/blogs/models.py
@@ -4,6 +4,11 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        "auth.User", on_delete=models.CASCADE, related_name="blogs"
+    )
     body = models.TextField()
     create_date = models.DateField(null=False, auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
