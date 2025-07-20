c0f712aca442aee1a3bd9bd81b43ef6440284c52
Gamoventure1175 <gauravmahajan1175@gmail.com>
2025-07-20 15:46:56 +0530

    using get_absolute_url() for creating url paths directly from the models

--- my_test_project/blogs/models.py
+++ my_test_project/blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -12,3 +13,6 @@ class Blog(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog_detail", kwargs={"pk": self.pk})
